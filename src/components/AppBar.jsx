986c9c5f93a026c62eb087414f2aeec29b63fa68
fakamda <facundoelcardenal@gmail.com>
2024-01-04 23:31:26 -0300

    routes and link between

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,27 +1,57 @@
 import React from "react"
-import { StyleSheet, View } from 'react-native'
+import { ScrollView, StyleSheet, View, TouchableWithoutFeedback } from 'react-native'
 import StyledText from "./StyledText"
 import Constants from 'expo-constants'
 import theme from "../theme.js"
+import { Link, useLocation } from "react-router-native"
 
 const styles = StyleSheet.create({
     container: {
+        flexDirection: 'row',
         backgroundColor: theme.appBar.primary,
         paddingTop: Constants.statusBarHeight + 10,
-        paddingBottom: 10,
-        paddingLeft: 10,
+        // paddingBottom: 10,
+        // paddingLeft: 10,
     },
     text: {
+        color: theme.appBar.textSecondary,
+        paddingHorizontal: 8
+    },
+    scroll: {
+        paddingBottom: 15
+    },
+    active: {
         color: theme.appBar.textPrimary
     }
 })
 
+const AppBarTab = ({ children, to }) => {
+    const { pathname } = useLocation()
+    const active = pathname === to
+
+    const textStyles = [
+        styles.text,
+        active && styles.active
+    ]
+
+    return (
+        <Link to={to} component={TouchableWithoutFeedback} >
+            <StyledText fontWeight='bold' style={textStyles}>
+                {children}
+            </StyledText>
+        </Link>
+    )
+}
+
+
 const AppBar = () => {
+    
     return (
         <View style={styles.container}>
-            <StyledText fontWeight='bold' style={styles.text}>
-                Repositories
-            </StyledText>
+            <ScrollView horizontal style={styles.scroll}>
+                <AppBarTab to='/'>Repositories</AppBarTab>
+                <AppBarTab to='/signin'>Sign In</AppBarTab>
+            </ScrollView>
         </View>
     )
 }
--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,8 @@
 const theme = {
     appBar: {
         primary: '#24292e',
-        textPrimary: '#fff'
+        textPrimary: '#fff',
+        textSecondary: '#777'
     },
     colors: {
         textPrimary: '#24292e',
