d338a23065bdfeaa7847d7f0367052b87a9597d9
fakamda <facundoelcardenal@gmail.com>
2024-01-06 20:30:50 -0300

    login form

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Formik } from 'formik'
+import { View, Button, StyleSheet } from 'react-native'
+import StyledTextInput from '../components/StyledTextInput'
+
+const initalValues = {
+    email: '',
+    password: ''
+}
+
+const styles = StyleSheet.create({
+    form: {
+        margin: 12
+    }
+})
 
 const LoginPage = () => {
   return (
-    <Text>Working on Login</Text>
+    <Formik initialValues={initalValues} onSubmit={values => console.log(values)}>
+        {({ handleChange, handleSubmit, values })=> {
+            return <View>
+                <StyledTextInput placeholder='E-mail' value={values.email} onChangeText={handleChange('email')} />
+                <StyledTextInput placeholder='Password' value={values.password} onChangeText={handleChange('password')} />
+                <Button onPress={handleSubmit} title='Log In' />
+            </View>
+        }}
+    </Formik>
   )
 }
 
